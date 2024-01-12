a9ba7567276529537a0cd7eff91c69be798a57b7
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-12 09:10:55 -0800

    Establish prevent default and variables in gatherUserInput function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 
-
-function suggestLanguage() {
+function gatherUserInput() {
 
 }
 
+
+function suggestLanguage(event) {
+
+}
+
+
+// Submit Button Event Listener
 function submitAnswers() {
     //Add Event Listener to Form
     form = document.querySelector("form");
-    form.addEventListener("submit", suggestLanguage);
+    form.addEventListener("submit", suggestLanguage());
 }
 
 
+// Clear Results Button
+
+
+// Window Load Event Listener
 window.addEventListener("load", submitAnswers);
